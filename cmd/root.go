26733d616affac2bbd9e0c96b6a5a2df42c12e2f
Hance08 <hancechin08@gmail.com>
2025-11-22 16:00:54 +0800

    Initialize application with config management and database setup

--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,86 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/hance08/kea/internal/logic/accounting"
+	"github.com/hance08/kea/internal/store"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-
+var (
+	cfgFile         string
+	dbStore         *store.Store
+	logic           *accounting.AccountingLogic
+	defaultCurrency string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "kea",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "kea is a CLI/TUI based personal accounting tool",
+	Long:  `kea is a CLI/TUI based personal accounting tool`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		// 1. read the config file
+		if err := initConfig(); err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		// 2. get config value
+		dbPath := viper.GetString("database.path")
+		if dbPath == "" {
+			return fmt.Errorf("error : can't find 'database.path' setting")
+		}
+
+		// expand the ~ symbol (if there has)
+		if strings.HasPrefix(dbPath, "~/") {
+			home, err := os.UserHomeDir()
+			if err != nil {
+				return fmt.Errorf("can't get the home directory : %w", err)
+			}
+			dbPath = filepath.Join(home, dbPath[2:])
+		}
+
+		// fmt.Printf("Database path : %s\n", dbPath)
+
+		migrationsPath := "file://./migrations"
+
+		defaultCurrency = viper.GetString("defaults.currency")
+		if defaultCurrency == "" {
+			return fmt.Errorf("error : can't find 'defaults.currency' setting")
+		}
+
+		// 3. initialize Store
+		var err error
+		dbStore, err = store.NewStore(dbPath, migrationsPath)
+		if err != nil {
+			return fmt.Errorf("failed to initialize database : %w", err)
+		}
+
+		// 4. initialize logic and input Store
+		logic = accounting.NewLogic(dbStore)
+
+		if err := ensureSystemAccounts(); err != nil {
+			return fmt.Errorf("failed to initialize system account : %w", err)
+		}
+
+		return nil
+	},
+	PersistentPostRunE: func(cmd *cobra.Command, args []string) error {
+		if dbStore != nil {
+			return dbStore.Close()
+		}
+		return nil
+	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -37,15 +89,57 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "set the config file path")
+	home, _ := os.UserHomeDir()
+	viper.SetDefault("database.path", filepath.Join(home, ".kea", "kea.db"))
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kea.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	viper.SetDefault("defaults.currency", "TWD")
 }
 
+func initConfig() error {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
 
+		viper.AddConfigPath(filepath.Join(home, ".config", "kea")) // Linux or Mac: ~/.config/kea
+		viper.AddConfigPath(filepath.Join(home, ".kea"))           // Fallback: ~/.kea
+		viper.SetConfigName("config")                              // config.yaml
+		viper.SetConfigType("yaml")
+	}
+
+	viper.SetEnvPrefix("KEA")
+	viper.AutomaticEnv() // allow using environment varibles to override
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// use default settings
+		} else {
+			// config file has been found, but failed to interpret
+			return err
+		}
+	}
+	return nil
+}
+
+func ensureSystemAccounts() error {
+	// ensure Equity:OpeningBalance existed
+	_, err := logic.GetAccountByName("Equity:OpeningBalances")
+	if err != nil {
+		// account doesn't exist, create it
+		_, err = logic.CreateAccount(
+			"Equity:OpeningBalances",
+			"C",
+			defaultCurrency,
+			"Opening Balances (System Account)",
+			nil,
+		)
+		if err != nil {
+			return fmt.Errorf("failed create 'Equity:OpeningBalances' account : %w", err)
+		}
+		// fmt.Printf("automatic create system account -> Equity:OpeningBalances")
+	}
+
+	return nil
+}

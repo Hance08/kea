2597fe6c60fe38fd25f63232c26e0b70acd08cc1
Hance08 <hancechin08@gmail.com>
2025-11-22 16:00:54 +0800

    Add account list command with filtering options

---
+++ cmd/list.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/hance08/kea/internal/store"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listType        string
+	listShowBalance bool
+	listShowHidden  bool
+	listTree        bool
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all accounts",
+	Long:  `List all accounts in the database with optional filters`,
+	RunE: func(cmd *cobra.Command, agrs []string) error {
+		var accounts []*store.Account
+		var err error
+
+		if listType != "" {
+			accounts, err = logic.ListAccountsByType(listType)
+		} else {
+			accounts, err = logic.ListAllAccounts()
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if !listShowHidden {
+			accounts = filterHiddenAccounts(accounts)
+		}
+
+		if listTree {
+			// TODO: displayAccountsTree(accounts, listShowBalance)
+		} else {
+			displayAccountsList(accounts, listShowBalance)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	accountCmd.AddCommand(listCmd)
+
+	listCmd.Flags().StringVarP(&listType, "type", "t", "", "Filter by account type (A, L, C, R, E)")
+	listCmd.Flags().BoolVarP(&listShowBalance, "balance", "b", true, "Show account balances")
+	listCmd.Flags().BoolVar(&listShowHidden, "show-hidden", false, "Show hidden accounts")
+	listCmd.Flags().BoolVar(&listTree, "tree", false, "Display as tree structure (not yet implemented)")
+}
+
+func filterHiddenAccounts(accounts []*store.Account) []*store.Account {
+	var filtered []*store.Account
+	for _, acc := range accounts {
+		if !acc.IsHidden {
+			filtered = append(filtered, acc)
+		}
+	}
+	return filtered
+}
+
+func displayAccountsList(accounts []*store.Account, showBalance bool) {
+	fmt.Println("Account List")
+	fmt.Println("--------------------------------------------------")
+	fmt.Printf("%-30s [%s] %s\n", "Name", "Type", "Currency")
+	fmt.Println("--------------------------------------------------")
+
+	for _, acc := range accounts {
+		fmt.Printf("%-30s [%s] %+6s\n", acc.Name, acc.Type, acc.Currency)
+
+		if showBalance {
+			balance, _ := logic.GetAccountBalanceFormatted(acc.ID)
+			fmt.Printf("  Balance: %s\n", balance)
+		}
+		fmt.Println("--------------------------------------------------")
+	}
+
+	fmt.Printf("Total: %d accounts\n", len(accounts))
+}

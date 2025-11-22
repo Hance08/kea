6a8381786b34bdb8ee7e70eddb56e58b273b01e6
Hance08 <hancechin08@gmail.com>
2025-11-22 16:00:54 +0800

    Update account command descriptions

--- cmd/account.go
+++ cmd/account.go
@@ -10,13 +10,8 @@ import (
 // accountCmd represents the account command
 var accountCmd = &cobra.Command{
 	Use:   "account",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "It can create, edit, delete account and show the list of all accounts.",
+	Long: `It can create, edit, delete account and show the list of all accounts.`,
 }
 
 func init() {

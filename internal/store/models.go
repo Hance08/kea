17732253d957bcc424e86995e2076a04a4aca873
Hance08 <hancechin08@gmail.com>
2025-11-22 23:53:52 +0800

    chore: format struct fields in models.go

--- internal/store/models.go
+++ internal/store/models.go
@@ -1,27 +1,32 @@
 package store
 
 type Account struct {
-	ID int64
-	Name string
-	Type string
-	ParentID *int64
-	Currency string
+	ID          int64
+	Name        string
+	Type        string
+	ParentID    *int64
+	Currency    string
 	Description string
-	IsHidden bool
+	IsHidden    bool
+}
+
+// Error implements error.
+func (a *Account) Error() string {
+	panic("unimplemented")
 }
 
 type Transaction struct {
-	ID int64
-	Timestamp int64
+	ID          int64
+	Timestamp   int64
 	Description string
-	Status int
+	Status      int
 }
 
 type Split struct {
-	ID int64
+	ID            int64
 	TransactionID int64
-	AccountID int64
-	Amount int64 
-	Currency string
-	Memo string
+	AccountID     int64
+	Amount        int64
+	Currency      string
+	Memo          string
 }

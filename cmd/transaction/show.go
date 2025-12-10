5153e3b362b48dd074d1dc14f32d6f6b18591d26
Hance08 <hancechin08@gmail.com>
2025-12-10 18:20:58 +0800

    refactor: introduce ShowCommandRunner in transaction/show.go

--- cmd/transaction/show.go
+++ cmd/transaction/show.go
@@ -9,24 +9,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type ShowCommandRunner struct {
+	svc *service.AccountingService
+}
+
 func NewShowCmd(svc *service.AccountingService) *cobra.Command {
 	return &cobra.Command{
 		Use:   "show <transaction-id>",
 		Short: "Show transaction details",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runTransactionShow(svc, args)
+			runner := &ShowCommandRunner{
+				svc: svc,
+			}
+			return runner.Run(args)
 		},
 	}
 }
 
-func runTransactionShow(svc *service.AccountingService, args []string) error {
+func (r *ShowCommandRunner) Run(args []string) error {
 	var txID int64
 	if _, err := fmt.Sscanf(args[0], "%d", &txID); err != nil {
 		return fmt.Errorf("invalid transaction ID: %s", args[0])
 	}
 
-	detail, err := svc.GetTransactionByID(txID)
+	detail, err := r.svc.GetTransactionByID(txID)
 	if err != nil {
 		pterm.Error.Printf("Failed to get transaction: %v\n", err)
 		return nil

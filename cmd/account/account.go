4cb49f47a34161747abb243e10e048a41f18fa1e
Hance08 <hancechin08@gmail.com>
2025-12-10 16:43:29 +0800

    refactor: convert account command to use constructor pattern

--- cmd/account/account.go
+++ cmd/account/account.go
@@ -5,29 +5,18 @@ package account
 
 import (
 	"github.com/hance08/kea/internal/service"
-	"github.com/hance08/kea/internal/validation"
 	"github.com/spf13/cobra"
 )
 
-var (
-	logic     *service.AccountingService
-	validator *validation.AccountValidator
-)
+func NewAccountCmd(svc *service.AccountingService) *cobra.Command {
+	accountCmd := &cobra.Command{
+		Use:   "account",
+		Short: "It can create, edit, delete account and show the list of all accounts.",
+		Long:  `It can create, edit, delete account and show the list of all accounts.`,
+	}
 
-// AccountCmd represents the account command
-var AccountCmd = &cobra.Command{
-	Use:   "account",
-	Short: "It can create, edit, delete account and show the list of all accounts.",
-	Long:  `It can create, edit, delete account and show the list of all accounts.`,
-}
+	accountCmd.AddCommand(NewListCmd(svc))
+	accountCmd.AddCommand(NewCreateCmd(svc))
 
-func init() {
-	AccountCmd.AddCommand(listCmd)
-	AccountCmd.AddCommand(createCmd)
-}
-
-// SetDependencies allows root command to inject dependencies
-func SetDependencies(l *service.AccountingService) {
-	logic = l
-	validator = validation.NewAccountValidator(l)
+	return accountCmd
 }

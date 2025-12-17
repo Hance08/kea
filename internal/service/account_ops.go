0eab076a028e372bc50f11adb8dbd35fb68ca46f
Hance08 <hancechin08@gmail.com>
2025-12-17 15:44:21 +0800

    refactor: split AccountService into query and operation files

--- internal/service/account_ops.go
+++ internal/service/account_ops.go
@@ -2,20 +2,10 @@ package service
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/hance08/kea/internal/store"
 )
-
-type AccountService struct {
-	repo   store.Repository
-	config Config
-}
-
-func NewAccountService(repo store.Repository, cfg Config) *AccountService {
-	return &AccountService{repo: repo, config: cfg}
-}
 
 func (as *AccountService) CreateAccount(name, accType, currency, description string, parentID *int64) (*store.Account, error) {
 	newID, err := as.repo.CreateAccount(name, accType, currency, description, parentID)
@@ -32,49 +22,6 @@ func (as *AccountService) CreateAccount(name, accType, currency, description str
 		ParentID:    parentID,
 		IsHidden:    false,
 	}, nil
-}
-
-func (as *AccountService) GetAllAccounts() ([]*store.Account, error) {
-	return as.repo.GetAllAccounts()
-}
-
-func (as *AccountService) GetAccountByName(name string) (*store.Account, error) {
-	return as.repo.GetAccountByName(name)
-}
-
-func (as *AccountService) CheckAccountExists(name string) (bool, error) {
-	return as.repo.AccountExists(name)
-}
-
-func (as *AccountService) GetAccountsByType(accType string) ([]*store.Account, error) {
-	return as.repo.GetAccountsByType(accType)
-}
-
-func (as *AccountService) GetAccountBalanceFormatted(accountID int64) (string, error) {
-	balance, err := as.repo.GetAccountBalance(accountID)
-	if err != nil {
-		return "", err
-	}
-
-	balanceFloat := float64(balance) / 100
-	return fmt.Sprintf("%.2f", balanceFloat), nil
-}
-
-func (as *AccountService) GetRootNameByType(accType string) (string, error) {
-	switch strings.ToUpper(accType) {
-	case "A":
-		return "Assets", nil
-	case "L":
-		return "Liabilities", nil
-	case "E":
-		return "Expenses", nil
-	case "R":
-		return "Revenue", nil
-	case "C":
-		return "Equity", nil
-	default:
-		return "", fmt.Errorf("invalid account type '%s' (must be A, L, C, R, E)", accType)
-	}
 }
 
 func (as *AccountService) SetBalance(account *store.Account, amountInCents int64) error {

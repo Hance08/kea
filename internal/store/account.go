37abf8701a07663afd7474509c678520dd36e322
Hance08 <hancechin08@gmail.com>
2025-12-17 15:44:21 +0800

    refactor: extract Account model to separate file

---
+++ internal/store/account.go
@@ -0,0 +1,11 @@
+package store
+
+type Account struct {
+	ID          int64
+	Name        string
+	Type        string
+	ParentID    *int64
+	Currency    string
+	Description string
+	IsHidden    bool
+}
--- internal/store/transaction.go
+++ internal/store/transaction.go
@@ -6,21 +6,6 @@ const (
 	StatusReconciled = 2
 )
 
-type Account struct {
-	ID          int64
-	Name        string
-	Type        string
-	ParentID    *int64
-	Currency    string
-	Description string
-	IsHidden    bool
-}
-
-// Error implements error.
-func (a *Account) Error() string {
-	panic("unimplemented")
-}
-
 type Transaction struct {
 	ID          int64
 	Timestamp   int64

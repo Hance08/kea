95fabdefe6341a21d9375dee1cccf3f0e3381b4d
Hance08 <hancechin08@gmail.com>
2025-12-25 00:11:16 +0800

    refactor(account): extract account creation logic to service layer

--- internal/service/account_ops.go
+++ internal/service/account_ops.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/hance08/kea/internal/model"
 )
 
@@ -19,4 +21,26 @@ func (as *AccountService) CreateAccount(name, accType, currency, description str
 		ParentID:    parentID,
 		IsHidden:    false,
 	}, nil
+}
+
+func (s *Service) CreateAccountWithBalance(name, accType, currency, description string, parentID *int64, balance int64) (*model.Account, error) {
+	account, err := s.Account.CreateAccount(name, accType, currency, description, parentID)
+	if err != nil {
+		return nil, err
+	}
+
+	if balance != 0 {
+		if err := s.Transaction.CreateOpeningBalance(account, balance); err != nil {
+			return account, fmt.Errorf("account created but failed to set opening balance: %w", err)
+		}
+	}
+
+	return account, nil
+}
+
+func (as *AccountService) FormatAccountName(prefix, name string) string {
+	if prefix == "" {
+		return name
+	}
+	return prefix + ":" + name
 }

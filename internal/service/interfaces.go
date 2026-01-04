d7cb393ef7a270ba02c75375e0f80be9c021c96b
Hance08 <hancechin08@gmail.com>
2026-01-04 20:59:12 +0800

    Move repository interfaces from store to service package

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -1,14 +1,14 @@
-package store
+package service
 
 import "github.com/hance08/kea/internal/model"
 
 type AccountRepository interface {
-	CreateAccount(name, accType, currency, description string, parentID *int64) (int64, error)
+	CreateAccount(name string, accType model.AccountType, currency, description string, parentID *int64) (int64, error)
 	GetAllAccounts() ([]*model.Account, error)
 	GetAccountByName(name string) (*model.Account, error)
 	GetAccountByID(id int64) (*model.Account, error)
 	AccountExists(name string) (bool, error)
-	GetAccountsByType(accType string) ([]*model.Account, error)
+	GetAccountsByType(accType model.AccountType) ([]*model.Account, error)
 	GetAccountBalance(accountID int64) (int64, error)
 }
 
@@ -19,9 +19,9 @@ type TransactionRepository interface {
 	GetTransactionsByDateRange(startTime, endTime int64) ([]*model.Transaction, error)
 	GetAllTransactions(limit int) ([]*model.Transaction, error)
 
-	UpdateTransactionStatus(txID int64, status int) error
+	UpdateTransactionStatus(txID int64, status model.TransactionStatus) error
 	DeleteTransaction(txID int64) error
-	UpdateTransactionBasic(txID int64, description string, timestamp int64, status int) error
+	UpdateTransactionBasic(txID int64, description string, timestamp int64, status model.TransactionStatus) error
 
 	CreateSplit(txID int64, split *model.Split) (int64, error)
 	UpdateSplit(splitID int64, accountID int64, amount int64, currency string, memo string) error

68109b5bb61553db6c5b0d41932cc7f447b02421
Hance08 <hancechin08@gmail.com>
2026-02-28 22:27:59 +0800

    refactor: introduce CommonView for shared view functionality

---
+++ internal/ui/views/common.go
@@ -0,0 +1,30 @@
+package views
+
+import "github.com/pterm/pterm"
+
+type CommonView struct {
+}
+
+func NewCommonView() *CommonView {
+	return &CommonView{}
+}
+
+func (v *CommonView) ShowError(msg string, err error) {
+	if err != nil {
+		pterm.Error.Printf("%s: %v\n", msg, err)
+	} else {
+		pterm.Error.Println(msg)
+	}
+}
+
+func (v *CommonView) ShowWarning(msg string) {
+	pterm.Warning.Println(msg)
+}
+
+func (v *CommonView) ShowSuccess(msg string) {
+	pterm.Success.Println(msg)
+}
+
+func (v *CommonView) ShowInfo(msg string) {
+	pterm.Info.Println(msg)
+}

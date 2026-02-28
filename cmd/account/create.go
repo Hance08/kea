991961ac5997172525a7542eee994d6ca879ff5b
Hance08 <hancechin08@gmail.com>
2026-02-28 22:27:59 +0800

    refactor: extract account creation types and interfaces to separate file

--- cmd/account/create.go
+++ cmd/account/create.go
@@ -16,29 +16,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type CreateView interface {
-	RenderSummary(data views.AccountSummaryItem) error
-	ShowSuccess(id int64, fullName string) error
-}
-
-type CreateProvider interface {
-	GetAllAccounts() ([]*model.Account, error)
-	GetAccountByName(name string) (*model.Account, error)
-	GetRootNameByType(accType string) (string, error)
-	CheckAccountExists(name string) (bool, error)
-	FormatAccountName(prefix string, name string) string
-	CreateAccountWithBalance(name string, accType model.AccountType, currency, description string, parentID *int64, balance int64) (*model.Account, error)
-}
-
-type createFlags struct {
-	Name        string
-	Type        string
-	Parent      string
-	BalanceStr  string
-	Currency    string
-	Description string
-}
-
 type createRunner struct {
 	name            string
 	fullName        string
---
+++ cmd/account/create_types.go
@@ -0,0 +1,29 @@
+package account
+
+import (
+	"github.com/hance08/kea/internal/model"
+	"github.com/hance08/kea/internal/ui/views"
+)
+
+type CreateView interface {
+	RenderSummary(data views.AccountSummaryItem) error
+	ShowSuccess(msg string)
+}
+
+type CreateProvider interface {
+	GetAllAccounts() ([]*model.Account, error)
+	GetAccountByName(name string) (*model.Account, error)
+	GetRootNameByType(accType string) (string, error)
+	CheckAccountExists(name string) (bool, error)
+	FormatAccountName(prefix string, name string) string
+	CreateAccountWithBalance(name string, accType model.AccountType, currency, description string, parentID *int64, balance int64) (*model.Account, error)
+}
+
+type createFlags struct {
+	Name        string
+	Type        string
+	Parent      string
+	BalanceStr  string
+	Currency    string
+	Description string
+}

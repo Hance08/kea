41488c4e23e21deb2670aaaffc153154f1069b37
Hance08 <hancechin08@gmail.com>
2026-05-23 21:41:21 +0800

    feat: add chunkInt64 helper for batching IN-clause IDs (#125)

---
+++ internal/store/chunk.go
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026  Hance Chin
+
+package store
+
+const sqliteChunkSize = 500
+
+func chunkInt64(ids []int64, size int) [][]int64 {
+	if len(ids) == 0 {
+		return nil
+	}
+	chunks := make([][]int64, 0, (len(ids)+size-1)/size)
+	for i := 0; i < len(ids); i += size {
+		end := i + size
+		if end > len(ids) {
+			end = len(ids)
+		}
+		chunks = append(chunks, ids[i:end])
+	}
+	return chunks
+}

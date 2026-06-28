a5f0edbbef4a7308bb01ca060bc05755ccda9aea
Hance08 <hancechin08@gmail.com>
2026-06-28 22:51:38 +0800

    feat(spa): redirect / to /dashboard

--- spa/src/routes/index.tsx
+++ spa/src/routes/index.tsx
@@ -2,15 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   beforeLoad: () => {
-    throw redirect({
-      to: '/balances',
-      search: {
-        a_offset: 0,
-        a_sort: 'balance_desc',
-        l_offset: 0,
-        l_sort: 'balance_desc',
-        view: 'cards',
-      },
-    });
+    throw redirect({ to: '/dashboard' });
   },
 });
